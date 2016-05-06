dd6dc2a98886abc593a6509b0783333292b262c2
verifiablyhuman <verifiablyhuman@gmail.com>
2016-05-06 16:31:18 -0600

    Updated Files
    Added the updated files which contain a fully functional battleship
    game.

--- game_methods.py
+++ game_methods.py
@@ -31,13 +31,38 @@ def get_spots(line, size):
     return spots
 
 
+def invert(board):
+    """This function will switch the rows and columns of the given board."""
+    inverted = []
+    for a in xrange(len(board[0])):
+        inverted.append([row[a] for row in board])
+    return inverted
+
+
 def player_wins(pboard, sboard):
-    pass
+    """This function tests the player board and ship board to see if the player has won."""
+    for prow, srow in zip(pboard, sboard):
+        for pcell, scell in zip(prow, srow):
+            if scell == 'S' and pcell != 'H':
+                return False
+    else:
+        return True
 
 
 def get_guess():
-    pass
+    print "Guess a place on the board. For row B, column 1 you would guess 'B1'."
+    raw_guess = raw_input("\t>")
+    char_int_map = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9}
+    row = char_int_map[raw_guess[0]]
+    col = int(raw_guess[1])
+    return (row, col)
 
 
-def check_hit(pboard, sboard):
-    pass
+def check_hit(guess, pboard, sboard):
+    row, col = guess
+    if sboard[row][col] == 'S':
+        pboard[row][col] = 'H'
+        print 'HIT!'
+    else:
+        pboard[row][col] = 'X'
+        print 'MISS!'
